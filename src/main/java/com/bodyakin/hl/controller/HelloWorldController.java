94a8982a63360d001308396aed0f11a0fd4dfffc
ebodyakin <ebodyakin@alfabank.ru>
2025-11-09 21:57:21 +0300

    add payload endpoint

--- src/main/java/com/bodyakin/hl/controller/HelloWorldController.java
+++ src/main/java/com/bodyakin/hl/controller/HelloWorldController.java
@@ -2,15 +2,46 @@ package com.bodyakin.hl.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
+    private static volatile double sink;
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World!";
+    public HelloDto hello() {
+        return new HelloDto("hello");
+    }
+
+    @GetMapping("/payload")
+    public PayloadDto payload(@RequestParam(defaultValue = "100") long cpuMsec) {
+        final long now = System.nanoTime();
+        final int cpuCycles = busyWork(cpuMsec);
+        final long end = System.nanoTime();
+        Duration duration = Duration.ofMillis((end - now) / 1_000_000);
+        return new PayloadDto(duration, cpuCycles);
+    }
+
+    private int busyWork(long timeMs) {
+        long end = System.nanoTime() + timeMs * 1_000_000;
+        double x = 0;
+        int cycesCount = 0;
+        while (System.nanoTime() < end) {
+            x += Math.sqrt(Math.random());
+            cycesCount++;
+        }
+        sink = x;
+        return cycesCount;
+    }
+
+    public record HelloDto(String value) {
+    }
+
+    public record PayloadDto(Duration wallTimeSec, int totalCycles) {
     }
 
 }
